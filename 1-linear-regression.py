d68a546223cfc1bc12a7cac25c184f4b95ce399e
ben <bin.go2011@yahoo.com>
2019-04-20 16:19:06 +0800

    pytorch version

--- 1-linear-regression.py
+++ 1-linear-regression.py
@@ -26,3 +26,33 @@ for epoch in range(100):
 print("my new prediction after training", forward(4))
 
 #%%
+import torch
+from torch.autograd import Variable
+
+x_data = [1.0, 2.0, 3.0]
+y_data = [2.0, 4.0, 6.0]
+
+w = Variable(torch.Tensor([1.0]), requires_grad=True)
+
+def forward(x):
+    return x*w
+
+def loss(x, y):
+    y_pred = forward(x)
+    return (y_pred-y)*(y_pred-y)
+
+print("my prediction before training:", 4, forward(4).data[0])
+
+for epoch in range(10):
+    for x_val, y_val in zip(x_data, y_data):
+        l = loss(x_val, y_val)
+        l.backward()
+        print("\tgrad: ", x_val, y_val, w.grad.data[0])
+        w.data = w.data - 0.01*w.grad.data;
+
+        w.grad.data.zero_()
+    print("my progress:", epoch, l.data[0])
+print("my prediction after training", forward(4).data[0])
+
+
+
