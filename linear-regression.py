cdf46266bf9723465e2579ff94a12f58450ccc63
ben <bin.go2011@yahoo.com>
2019-04-23 11:25:06 +0800

    linear regration implementation by numpy, scikit-learn, pytorch

---
+++ linear-regression.py
@@ -0,0 +1,68 @@
+#%%
+import numpy as np
+
+X = 2 * np.random.rand(100, 1)
+y = 4 + 3 * X + np.random.randn(100, 1)
+
+# close-form
+X_b = np.c_[np.ones((100, 1)), X]
+theta_best = np.linalg.inv(X_b.T.dot(X_b)).dot(X_b.T).dot(y)
+
+theta_best
+
+
+#%%
+X_new = np.array([[0], [2]])
+X_new_b = np.c_[np.ones((2, 1)), X_new]
+y_predict = X_new_b.dot(theta_best)
+y_predict
+
+#%%
+plt.plot(X_new, y_predict, "r-")
+plt.plot(X, y, "b.")
+plt.axis([0, 2, 0, 15])
+plt.show()
+
+#%%
+from sklearn.linear_model import LinearRegression
+
+lin_reg = LinearRegression()
+lin_reg.fit(X, y)
+
+lin_reg.intercept_, lin_reg.coef_
+# lin_reg.predict(X_new)
+
+#%%
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+
+x_data = 2 * torch.rand(100,1)
+y_data = 4 + 3 * x_data + torch.randn(100, 1)
+
+class LinearModel(nn.Module):
+    def __init__(self, in_dim, out_dim):
+        super(LinearModel, self).__init__()
+        self.linear = nn.Linear(in_dim, out_dim)
+    
+    def forward(self, x):
+        out = self.linear(x)
+        return out
+
+model = LinearModel(1,1)
+
+criterion = torch.nn.MSELoss()
+optimizer = torch.optim.SGD(model.parameters(), lr=0.01)
+
+for epoch in range(1000):
+    y_pred = model(x_data)
+
+    loss = criterion(y_pred, y_data)
+    # print(epoch, loss.data)
+    optimizer.zero_grad()
+    loss.backward()
+    optimizer.step()
+
+model.state_dict()
+
+#%%
